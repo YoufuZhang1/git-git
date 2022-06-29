b6db51b866598cd9406cc34e4d1a38b2cf1b6008
zyf <2733024658@qq.com>
2022-06-29 17:47:08 +0800

    4 commit

--- src/main/java/Stu.java
+++ src/main/java/Stu.java
@@ -1,11 +1,9 @@
-/**
- * @author zyf
- * @date 2022年06月29日17:31时
- */
+
 public class Stu {
     public static void main(String[] args) {
         System.out.println("学习git的第一天。。。");
         System.out.println("学习git的第二天。。。");
         System.out.println("学习git的第三天。。。");
+        System.out.println("学习git的第4天。。。");
     }
 }
